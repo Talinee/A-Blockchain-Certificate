26d54228329e73ed2ecb0e1536ac8cd06adc950c
Toey <talinee_l@cmu.ac.th>
2021-02-22 15:19:53 +0700

    edit

--- src/api/rest/routes/product.ts
+++ src/api/rest/routes/product.ts
@@ -1,72 +1,75 @@
-import { Response,NextFunction, Router ,Request, response} from 'express'
+import { Response, NextFunction, Router, Request, response } from 'express'
 import { request } from 'http'
 import productService from 'services/product'
 
 const router: Router = Router()
 
-router.post('/product', async(request,response: Response, next: NextFunction) => {
+router.post('/product', async(request, response: Response, next: NextFunction) => {
     try {
         const input = request.body
-        
+
         const product = await productService.add({
             name: input.name,
             img: input.img,
             code: input.code,
-            price: input.price
+            price: input.price,
         })
 
-        response.json(product);
+        response.json(product)
     }
-    catch(err) {
+    catch (err) {
         next(err)
     }
 })
 
-router.put('/product/:id', async(request,response: Response, next: NextFunction) => {
-    try {    
+router.post('/product/:id', async(request, response: Response, next: NextFunction) => {
+    try {
         const input = request.body
-        console.log(request.body);
-        
+        console.log(request.body)
+
         const params = request.params
         const product = await productService.update({
-                name: input.name,
-                img: input.img,
-                code: input.code,
-                price: input.price,
-                id: params.id
+            name: input.name,
+            img: input.img,
+            code: input.code,
+            price: input.price,
+            id: params.id,
         })
         response.json(product)
     }
-        catch(err) {
-            next(err)
-        }
-})
-
-router.get('/product/:id', async(request,response:Response,next:NextFunction) => {
-    try {
-        const product = await productService.get(request.params)
-        response.json(product)
-    }
-    catch(err) {
+    catch (err) {
         next(err)
     }
 })
 
-router.get('/product', async(request,response: Response, next: NextFunction) => {
+router.get('/product/:id', async(request, response: Response, next: NextFunction) => {
+    try {
+        const params = request.params.id
+        const product = await productService.get(params)
+        response.json(product)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.get('/product', async(request, response: Response, next: NextFunction) => {
     try {
         const product = await productService.getAll()
         response.json(product)
     }
-    catch(err){
+    catch (err) {
         next(err)
     }
 })
 
-router.delete('/product/:id', async(request,response: Response, next: NextFunction) => {
+router.post('/product/:id', async(request, response: Response, next: NextFunction) => {
     try {
-        const product = await productService.delete(request.params)
+        const params = request.params.id
+        const product = await productService.delete(params)
         response.status(204).end()
-    } catch (error) {
+    }
+    catch (error) {
         next(err)
     }
 })
--- src/services/product.ts
+++ src/services/product.ts
@@ -1,52 +1,44 @@
 import { Product, productDoc } from 'models/Product'
 
 const productService = {
-    async add(input): Promise<{ successful: boolean, message: String }> {
-        try {
-                const product: productDoc = new Product()
-                product.productName = input.name
-                product.productImg = input.img
-                product.productCode = input.code
-                product.price = input.price
-                await product.save()
+    async add(input): Promise<{ successful: boolean, message: String, }> {
+        const product: productDoc = new Product()
+        product.productName = input.name
+        product.productImg = input.img
+        product.productCode = input.code
+        product.price = input.price
+        await product.save()
+        return { successful: true, message: 'Success' }
 
-                return { successful: true, message: 'Success' }   
-        } catch(e) {
-            return {successful: false, message: 'error'}
-        }
     },
 
-    async get(input): Promise<{ successful: boolean, data: productDoc[] }> { 
-        const product = await Product.findOne({_id:input.id}).exec()
-        
+    async get(input): Promise<{ successful: boolean, data: productDoc, }> {
+        const product = await Product.findById({ _id: input.id }).exec()
+
         return { successful: true, data: product }
     },
 
-    async getAll(): Promise<{ successful: boolean, data: productDoc[] }> {        
+    async getAll(): Promise<{ successful: boolean, data: productDoc[], }> {
         const products = await Product.find()
-        
+
         return { successful: true, data: products }
     },
 
-    async update(input): Promise<{ successful: boolean, data: productDoc[]}> {
-            const product = await Product.findByIdAndUpdate(
-                {_id:input.id},
-                {$set: {    name:input.name, 
-                            img:input.img,
-                            code:input.code, 
-                            price:input.price
-                }},
-            )
-            return { successful: true, data: product }
+    async update(input): Promise<{ successful: boolean, data: productDoc, }> {
+        const product = await Product.findByIdAndUpdate(
+            { _id: input.id },
+            { $set: { name: input.name,
+                img: input.img,
+                code: input.code,
+                price: input.price,
+            } },
+        )
+        return { successful: true, data: product }
     },
 
-    async delete(input):Promise<{ successful: boolean, message: String }> {
-       try {
-        const products = await Product.findByIdAndDelete({_id:input.id})
-        return { successful: true, message: 'Success' }  
-       } catch (e) {
-        return {successful: false, message: 'error'}
-       }
-    }
+    async delete(input): Promise<{ successful: boolean, message: String, }> {
+        const product = await Product.findByIdAndDelete({ _id: input.id })
+        return { successful: true, message: 'Success' }
+    },
 }
 export default productService
